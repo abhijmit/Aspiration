eae08b0249aa252e86ae0c1d3f2640570450117e
Abhijit Mitra <abhijit.mitra@specsavers.com>
2018-08-29 15:57:35 +0100

    Remove directory

--- HandsOnSpringTest/src/main/java/com/example/demo/cdi/SomeCdiDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.cdi;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SomeCdiDao {
-
-}
--- HandsOnSpringTest/src/main/java/com/example/demo/data/DataLayer.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.data;
-
-import java.util.TimeZone;
-public interface DataLayer {
-
-	abstract String formatDate(String strFormat , String  strtmZn);
-	
-}
